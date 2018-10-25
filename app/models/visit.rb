da299c8cea2c264c3b36483f76c667d612b512cb
Peter Dietz <pdietz@onshift.com>
2018-10-25 02:18:33 -0400

    Small reformating and reusing code

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -5,4 +5,8 @@ class Visit < ApplicationRecord
   def display_date
     return "#{self.scheduled_start.strftime("%m/%d/%Y @ %I:%M%p")} - #{self.scheduled_end.strftime("%I:%M%p")}"
   end
+
+  def duration
+    return ((self.scheduled_end - self.scheduled_start) / 1.minutes).to_i
+  end
 end
